ac5e089092f180215ad450e6b49899bc3738179a
Yesi Meza <yesi.meza10@gmail.com>
2021-05-12 22:13:31 -0600

    updated relation to manager to be optional

--- app/models/vacation_request.rb
+++ app/models/vacation_request.rb
@@ -4,7 +4,7 @@ class VacationRequest < ApplicationRecord
                         :vacation_end_date
 
   belongs_to :employee
-  belongs_to :manager
+  belongs_to :manager, optional: true
 
   enum status: [:approved, :rejected, :pending]
 end
