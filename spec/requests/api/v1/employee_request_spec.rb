c9d1abb9cf2f1b65cdfc54e55c9a2d8730d3b680
Yesi Meza <yesi.meza10@gmail.com>
2021-05-12 22:12:54 -0600

    added more robust testing for post response expectation

--- spec/requests/api/v1/employee_request_spec.rb
+++ spec/requests/api/v1/employee_request_spec.rb
@@ -8,13 +8,19 @@ describe "Employee Vacation Request Endpoint" do
       query_params = ({
                        vacation_start_date: "07/04/2021",
                        vacation_end_date: "07/06/2021",
-                       employee_id: employee1.id
+                       employee_id: employee1.id, 
+                       status: "pending"
                       })
 
-      post "/api/v1/vacation_requests", params: query_params 
-      
+      post "/api/v1/vacation_request", params: query_params 
+      created_vacation = VacationRequest.last
 
-      expect(response).to be_succesful
+      expect(response).to be_successful
+
+      expect(created_vacation.status).to eq(query_params[:status])
+      expect(created_vacation.vacation_start_date).to eq(query_params[:vacation_start_date])
+      expect(created_vacation.vacation_end_date).to eq(query_params[:vacation_end_date])
+      expect(created_vacation.employee_id).to eq(query_params[:employee_id])
     end
   end
 end
